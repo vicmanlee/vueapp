bfae92277ac4be9c5e2fbda57130808c6d97dc3d
李凯 <likai_ms@outlook.com>
2017-10-03 19:17:36 +0800

    create files

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 5987,
+    port: 8080,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
