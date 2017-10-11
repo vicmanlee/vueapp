4a30b5fa09bb5d1170b3b11e70a21927a2174cdf
李凯 <likai_ms@outlook.com>
2017-10-12 06:51:12 +0800

    updata

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -10,7 +10,32 @@
         </li>
       </ul>
     </div>
-    <div class="goods-wrapper"></div>
+    <div class="foods-wrapper">
+      <ul>
+        <li v-for="item in goods" :key="item.name" class="food-list" >
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="food in item.foods" :key="food.name" class="food-item">
+              <div class="icon">
+                <img :src="food.icon" >
+              </div>
+              <div class="content">
+                <h2 class="name"></h2>
+                <p class="desc"></p>
+                <div class="extra">
+                  <span>月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span>￥{{food.price}}</span>
+                  <span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+               </div>
+            </li>
+          </ul>
+        </li>
+      </ul> 
+    </div>
   </div>
 </template>
 <script type="text/ecmascript-6">
@@ -80,7 +105,23 @@ export default {
           vertical-align middle
           border-bottom 1px solid rgba(7,17,27,0.1)
           font-size 12px
-    .goods-wrapper
+    .foods-wrapper
       flex 1
+      .title
+        padding-left 14px
+        height 26px
+        line-height 26px
+        border-left 2px solid #d9dde1
+        font-size 12px
+        color rgb(147,153,159)
+        background #f3f5f7
+      .food-item
+        display flex
+        margin 18px
+        padding-bottom 18px
+        border-bottom 1px solid rgba(7,17,27,0.1)
+        &:lastchild
+          border-none()
+          margin-bottom 0
 </style>
 
